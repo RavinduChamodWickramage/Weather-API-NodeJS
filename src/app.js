57b18cbfed277b0f58148a0f7bdf8264b3a138e2
Ravindu Chamod Wickramage <ravinduchamod1@gmail.com>
2025-03-17 01:30:02 +0530

    refactor[app-server]: update app and server configuration

--- src/app.js
+++ src/app.js
@@ -1,10 +1,36 @@
 const express = require("express");
-const connectDB = require("./config/db");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const errorHandler = require("./middlewares/errorHandler");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const weatherRoutes = require("./routes/weatherRoutes");
 
 const app = express();
 
-connectDB();
-
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/weather", weatherRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Weather Report API is running",
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+app.use(errorHandler);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
+const scheduleWeatherJob = require("./jobs/weatherJob");
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+scheduleWeatherJob();
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
